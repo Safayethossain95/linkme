c5dbb68039b36f779346b3c525a573240d9b33e5
Safayethossain95 <safayet.hossain95@gmail.com>
2021-09-02 22:06:40 +0600

    first

---
+++ js/custom.js
@@ -0,0 +1,97 @@
+var links= document.getElementById("demo2");
+          
+var first_click = true;
+links.onclick = function() {
+if (first_click) {
+     document.getElementById("demo2").innerHTML = "https://www.youtube.com/" ;
+     first_click = false;
+} else {
+     document.getElementById("demo2").innerHTML = "Show link" ;
+     first_click=true;
+}
+}
+
+var links2= document.getElementById("demo");
+
+var first_click = true;
+links2.onclick = function() {
+if (first_click) {
+     document.getElementById("demo").innerHTML = "https://google.com" ;
+     first_click = false;
+} else {
+     document.getElementById("demo").innerHTML = "Show link" ;
+     first_click=true;
+}
+}
+var links3= document.getElementById("demo3");
+
+var first_click = true;
+links3.onclick = function() {
+if (first_click) {
+     document.getElementById("demo3").innerHTML = "https://facebook.com" ;
+     first_click = false;
+} else {
+     document.getElementById("demo3").innerHTML = "Show link" ;
+     first_click=true;
+}
+}
+var links4= document.getElementById("demo4");
+
+var first_click = true;
+links4.onclick = function() {
+if (first_click) {
+     document.getElementById("demo4").innerHTML = "https://www.google.com/search?q=pastel+color+codes&client=firefox-b-d&sxsrf=ALeKk02z9zyNHdYH1txluHtf4gkbG7NCTQ:1627377318303&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiS4uP39ILyAhUk7XMBHe54AToQ_AUoAXoECAEQAw&biw=1366&bih=615#imgrc=1pbmTdY_MDTX0M" ;
+     first_click = false;
+} else {
+     document.getElementById("demo4").innerHTML = "Show link" ;
+     first_click=true;
+}
+}
+var links1= document.getElementById("demo5");
+
+var first_click = true;
+links1.onclick = function() {
+if (first_click) {
+     document.getElementById("demo5").innerHTML = "https://docs.google.com/spreadsheets/d/1USKNT6cN4FM7C5HN2tw0r7LOr2jA1CyUm8AK1sSh6G4/edit#gid=0" ;
+     first_click = false;
+} else {
+     document.getElementById("demo5").innerHTML = "Show link" ;
+     first_click=true;
+}
+}
+ var links5= document.getElementById("demo6");
+
+var first_click = true;
+links5.onclick = function() {
+if (first_click) {
+     document.getElementById("demo6").innerHTML = "https://stackoverflow.com/questions/4561097/css-box-shadow-bottom-only" ;
+     first_click = false;
+} else {
+     document.getElementById("demo6").innerHTML = "Show link" ;
+     first_click=true;
+}
+}
+var links6= document.getElementById("demo7");
+
+var first_click = true;
+links6.onclick = function() {
+if (first_click) {
+     document.getElementById("demo7").innerHTML = "https://tawk.to/" ;
+     first_click = false;
+} else {
+     document.getElementById("demo7").innerHTML = "Show link" ;
+     first_click=true;
+}
+}
+var links7= document.getElementById("demo8");
+
+var first_click = true;
+links7.onclick = function() {
+if (first_click) {
+     document.getElementById("demo8").innerHTML = "https://tawk.to/" ;
+     first_click = false;
+} else {
+     document.getElementById("demo8").innerHTML = "Show link" ;
+     first_click=true;
+}
+}
